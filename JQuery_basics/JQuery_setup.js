758ef2e673452134d394ee65cedf2d8255facf07
Brenasia <breeholiman@yahoo.com>
2017-06-27 09:13:58 -0400

    added files

--- JQuery_basics/JQuery_setup.js
+++ JQuery_basics/JQuery_setup.js
@@ -1,3 +1,20 @@
-(document).ready(function function_name() {
-	alert("JQuery is working!")
+$(document).ready(function() {
+	$("#hide").click(function(){
+		$("#image").hide();
+	});
+	$("#show").click(function(){
+		$("#image").show();
+	})
+	$(explode).click(function(){
+		$("#image").hide("explode", {pieces:16}, 2000);
+	})
+	$("#fadein").click(function(){
+		$("#image").fadein(3000)
+	})
+	$("#fadeout").click(function(){
+		$("#image").fadeout(3000)
+	})
+
+
 });
+
---
+++ JQuery_basics/songlist.js
@@ -0,0 +1,15 @@
+alert("hey");
+
+$(document).ready(function() {
+	$("#add").on("click", function(){
+		var song = $("#song").val();
+		console.log(song);
+
+		var listItem = "<li>" + song + "</li>";
+		console.log(listItem)
+
+		$("#song-list").append(listItem);
+		$("#song").val("");
+	})
+	
+});
